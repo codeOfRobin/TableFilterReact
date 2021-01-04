a47357c568f2ed34a7f796ec80ce3e97fc2a32fc
Robin Malhotra <malhotra.robin@yahoo.co.in>
2021-01-04 00:06:34 +0000

    Need to add data

---
+++ src/components/OurTable.js
@@ -0,0 +1,24 @@
+import React from "react"
+import data from "../data/data.json"
+
+const OurTable = () => {
+    console.log(data.results)
+    return (
+    <table>
+        <th>
+            Name
+        </th>
+        <th>
+            Planet
+        </th>
+
+        <tr>
+            <td>
+                
+            </td>
+        </tr>
+    </table>
+    )
+}
+
+export default OurTable
